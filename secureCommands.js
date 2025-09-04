0666c04b18999a2890d096216a40fdff1462f48b
Fermento Punk <159047095+Punkxbass@users.noreply.github.com>
2025-09-04 17:39:24 -0500

    fix(core): Solución de errores críticos de instalación y permisos

--- secureCommands.js
+++ secureCommands.js
@@ -4,7 +4,7 @@ const allowedCommands = {
   RESTART_SERVER: { cmd: '/bin/systemctl', args: ['restart', 'minecraft.service'] },
   INSTALL_PAPER: { cmd: '/home/ubuntu/install_paper.sh', args: [] },
   BAN_PLAYER: { cmd: '/usr/local/bin/mc-ban-player', args: ['--player'] },
-  REBOOT_VPS: { cmd: '/sbin/reboot', args: [] }
+  REBOOT_VPS: { cmd: '/usr/bin/sudo', args: ['/sbin/reboot'] }
 };
 
 module.exports = allowedCommands;
